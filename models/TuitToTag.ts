064b78640ad583f55b578ed901f12090c3c732e4
Laura Culligan <lgculligan@gmail.com>
2022-02-02 17:14:35 -0500

    commiting models

---
+++ models/TuitToTag.ts
@@ -0,0 +1,6 @@
+import Tuit from "./Tuit";
+
+export default class TuitToTag {
+    private tuit: Tuit | null = null;
+    private tag: string = "";
+}
---
+++ models/TuitToTopic.ts
@@ -0,0 +1,6 @@
+import Tuit from "./Tuit";
+
+export default class TuitToTopic {
+    private tuit: Tuit | null = null;
+    private topic: string = "";
+}
