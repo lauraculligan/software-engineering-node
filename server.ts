24e73c6f43be295260fa06fc6ace62787c73e31f
Laura Culligan <lgculligan@gmail.com>
2022-03-26 23:29:43 -0400

    added proxy and sameSite

--- server.ts
+++ server.ts
@@ -36,8 +36,10 @@ let sess = {
     secret: SECRET,
     saveUninitialized: true,
     resave: true,
+    proxy: true,
     cookie: {
-        secure: false
+        secure: true,
+        sameSite: 'none'
     }
 }
 
